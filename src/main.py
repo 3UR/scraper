21f5295206c3b4eb1b3ab4c17fd6e33e220ba47d
igna <i@asylum.rest>
2023-04-03 03:01:48 +0000

    yep

--- src/main.py
+++ src/main.py
@@ -1,41 +1,23 @@
 import os
 import sys
-import time
 import selfcord as discord
 import aiohttp
 import aiofiles
 import contextlib
-from discord_webhook import DiscordWebhook
 import random
 import asyncio
-
+from colorama import init, Fore
 
 class ConsoleUtils:
     """
-    a class that contains a bunch of console utils such as colors, title and clear console
+    a class that contains a bunch of console utils like title and clear console
 
-    TODO: 
+    TODO: #9 (move this to it's own module.)
     """
 
     def __init__(self) -> None:
         
         pass
-
-    class ConsoleColors:
-        """a class that contains a bunch of console colors."""
-
-        RESET = '\033[0m'
-        BLACK = '\033[30m'
-        RED = '\033[31m'
-        GREEN = '\033[32m'
-        YELLOW = '\033[33m'
-        BLUE = '\033[34m'
-        MAGENTA = '\033[35m'
-        CYAN = '\033[36m'
-        WHITE = '\033[37m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
-
     @staticmethod
     def clear_console() -> None:
         """clears the console."""
@@ -77,22 +59,21 @@ class ConsoleUtils:
 
 
 ConsoleUtils = ConsoleUtils()
-ConsoleColors = ConsoleUtils.ConsoleColors
-
+init(convert=True)
 
 ConsoleUtils.clear_file('images.txt')
 ConsoleUtils.delete_files(confirm=True)
 ConsoleUtils.set_console_title('discord img scraper | by @obstructive')
 ConsoleUtils.clear_console()
 token = input(
-    f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Enter token: '
+    f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Enter token: '
 )
 client = discord.Client()
 
 
 async def scrape_channel():
     channel_id = input(
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Enter channel id: '
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Enter channel id: '
     )
     channel = await client.fetch_channel(channel_id)
     async for message in channel.history(limit=None):
@@ -118,10 +99,10 @@ async def scrape_channel():
                 ):
                     
                     with open('images.txt', 'a', encoding='utf-8') as f:
-                        f.write(attachment.url + '\n')
+                        f.write(f'{attachment.url}\n')
                     
                     print(
-                        f'{ConsoleUtils.ConsoleColors.MAGENTA}[{ConsoleUtils.ConsoleColors.RESET}~{ConsoleUtils.ConsoleColors.MAGENTA}]{ConsoleUtils.ConsoleColors.RESET} {attachment.url}'
+                        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} {attachment.url}'
                     )
                 
 
@@ -135,7 +116,7 @@ async def scrape_channel():
 
 async def send_to_channel():
     channel_id = input(
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Enter channel id: '
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Enter channel id: '
     )
     channel = await client.fetch_channel(channel_id)
     with open('images.txt', 'r') as f:
@@ -150,7 +131,7 @@ async def send_to_channel():
                     f = await aiofiles.open(f'file.{format}', mode='wb')
                     await f.write(await r.read())
                     await f.close()
-                    with contextlib.suppress(discord.errors.HTTPException):
+                    try:
                         filename = os.urandom(16).hex()
                         await channel.send(
                             file=discord.File(
@@ -158,9 +139,11 @@ async def send_to_channel():
                                 filename=f'{filename}.{format}',
                             )
                         )
+                    except discord.errors.HTTPException:
+                        pass
                     os.remove(f'file.{format}')
                     print(
-                        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} {line.strip()}'
+                        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} {line.strip()}'
                     )
                     ConsoleUtils.clear_file('images.txt')
 
@@ -168,7 +151,7 @@ async def send_to_channel():
 
 async def send_to_webhook():
     webhook_url = input(
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Enter Webhook Url: '
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Enter Webhook Url: '
     )
     with open('images.txt', 'r', encoding='UTF-8') as f:
         for line in f:
@@ -181,28 +164,23 @@ async def send_to_webhook():
                     f = await aiofiles.open(f'file.{format}', mode='wb')
                     await f.write(await r.read())
                     await f.close()
-                    webhook = DiscordWebhook(
-                        url=webhook_url,
-                        content=None,
-                        rate_limit_retry=True,
-                        allowed_mentions=None,
+                    webhook = discord.Webhook.from_url(
+                        webhook_url, adapter=discord.AsyncWebhookAdapter(session)
                     )
-                    random = os.urandom(16).hex()
-                    from io import BytesIO
-
-                    file = BytesIO(await r.read())
-
-                    webhook.add_file(
-                        filename=f'{random}.{format}', file=file.read()
-                    )
-                    webhook.execute()
-                    time.sleep(3)
-                    file.close()
+                    with contextlib.suppress(discord.errors.HTTPException):
+                        filename = os.urandom(16).hex()
+                        await webhook.send(
+                            file=discord.File(
+                                f'file.{format}',
+                                filename=f'{filename}.{format}',
+                            )
+                        )
                     os.remove(f'file.{format}')
                     print(
-                        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} {line.strip()}'
+                        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} {line.strip()}'
                     )
                     ConsoleUtils.clear_file('images.txt')
+
 
 
 async def menu():
@@ -211,57 +189,57 @@ async def menu():
     ConsoleUtils.clear_console()
     print(
         
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} 1. Scrape Channel'
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} 2. Send to Channel'
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} 3. Send to Webhook'
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} 4. Credits'
-        f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} 5. Exit'
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} 1. Scrape Channel\n'
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} 2. Send to Channel\n'
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} 3. Send to Webhook\n'
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} 4. Credits\n'
+        f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} 5. Exit'
     )
     while True:
         choice = input(
-            f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Enter Choice: '
+            f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Enter Choice: '
         )
         if choice == '1':
             await scrape_channel()
             print(
-                f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Finished Scraping Channel'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Finished Scraping Channel'
             )
-            time.sleep(1)
+            await asyncio.sleep(1)
             await menu()
         elif choice == '2':
             await send_to_channel()
             print(
-                f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Finished Sending Scraped Avatars/GIFs to Channel'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Finished Sending Scraped attachments to Channel'
             )
-            time.sleep(3)
+            await asyncio.sleep(3)
             await menu()
         elif choice == '3':
             await send_to_webhook()
             print(
-                f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Finished Sending Scraped Avatars/GIFs to Webhook'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Finished Sending Scraped attachments to Webhook'
             )
-            time.sleep(3)
+            await asyncio.sleep(3)
             await menu()
         elif choice == '4':
             print(
-                f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Credits: '
-                f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Made by: {{C.RESET}}igna'
-                f'{ConsoleColors.MAGENTA}[{ConsoleColors.RESET}~{ConsoleColors.MAGENTA}]{ConsoleColors.RESET} Github: {{C.RESET}}obstructive'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Credits:\n'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Made by: {{C.RESET}}igna#0002\n'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Github: {{C.RESET}}obstructive'
             )
-            asyncio.sleep(3)
+            await asyncio.sleep(3)
             await menu()
         elif choice == '5':
             print(
-                '\x1b[38;5;199m[{{C.RESET}}~\x1b[38;5;199m] {{C.RESET}}Exiting...'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Exiting...'
             )
-            time.sleep(1)
+            await asyncio.sleep(1)
             ConsoleUtils.clear_console()
             sys.exit()
         else:
             print(
-                '\x1b[38;5;199m[{{C.RESET}}~\x1b[38;5;199m] {{C.RESET}}Invalid Choice'
+                f'{Fore.MAGENTA}[{Fore.RESET}~{Fore.MAGENTA}]{Fore.RESET} Invalid Choice'
             )
-            time.sleep(1)
+            await asyncio.sleep(1)
             await menu()
 
 
